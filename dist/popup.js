f2dce854eecada5040987238bfab1aee44b53dd3
hihumikan <a@qqey.net>
2023-06-25 16:15:20 +0900

    ✨feat: typescript build

---
+++ dist/popup.js
@@ -0,0 +1,7 @@
+import { HELLO } from './constants.js';
+const $body = document.querySelector('body');
+$p.innerHTML = `${HELLO} Popup`;
+$p.innerHTML = 'Hello Popup';
+if ($body) {
+    $body.appendChild($p);
+}
